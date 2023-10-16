6d1e94183e14af5545c4c9600c80b0c5ec787d07
Saurav Raj <saurav.raj5592+git@gmail.com>
2023-10-16 21:19:30 +0530

    local change

--- project/classes/Surface.py
+++ project/classes/Surface.py
@@ -1,22 +1,18 @@
 class Surface:
-    def __init__(self, row: int, col: int, theta_dep: int, average_height: int = 0, total_particle: int = 0, max_height: int = 0) -> None:
+    def __init__(self, row: int, col: int, theta_dep: int, max_height: int = 0) -> None:
         self.row = row  # get input from user
         self.col = col  # get input from user
         self.theta_dep = theta_dep  # input from user
-        self.average_height = average_height
-        self.total_particle = total_particle
-        self.max_height = max_height
-        self.grid = [[0 for _ in range(col)] for _ in range(row)]
+        self._average_height = 0
+        self._total_particle = 0
+        self._max_height = max_height
+        self._grid = [[0 for _ in range(col)] for _ in range(row)]
 
     def get_average_height(self) -> float:
-        return self.average_height
-
-    def set_average_height(self) -> None:
-        self.average_height = self.get_total_particle() / (self.row * self.col)
+        return self.get_total_particle() / (self.row * self.col)
 
     def set_total_particle(self) -> None:
-        self.total_particle = self.get_total_particle() + self.theta_dep
-        self.set_average_height()
+        self._total_particle = self.get_total_particle() + self.theta_dep
 
     def get_total_particle(self) -> int:
-        return self.total_particle
+        return self._total_particle
--- scripts/main.py
+++ scripts/main.py
@@ -2,7 +2,7 @@ from project.classes.Surface import Surface
 
 s = Surface(4, 5, 10)
 
-for _ in range(10) :
+for _ in range(10):
     s.set_total_particle()
 
-print(s.get_average_height())   # prints 5.0 -- working
+print(s.get_average_height())
